aba57201de8565fd657d56dc4daf8c4e39744c50
Pablo <pablo.gim.fan@gmail.com>
2013-05-18 18:42:08 +0200

    Eliminados los comentarios estúpidos

--- pecera.ino
+++ pecera.ino
@@ -1,17 +1,13 @@
-/*
-
-3 5 9 11
-
- */
+/* Pines PWM usados.Cuidado al conectarlos -> 3 5 9 11 */
  
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
+
+// names:
 int ledA = 3;
 int ledB = 6;
 int ledC = 9;
 int ledD = 11;
 
-// the setup routine runs once when you press reset:
+
 void setup() {                
   // initialize the digital pin as an output.
   pinMode(ledA, OUTPUT);
@@ -20,29 +16,25 @@ void setup() {
   pinMode(ledD, OUTPUT);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-//  digitalWrite(ledA, HIGH);   // turn the LED on (HIGH is the voltage level)
-//  delay(1000);      
-//  digitalWrite(ledB, HIGH);   // turn the LED on (HIGH is the voltage level)
-//  delay(1000);
-  analogWrite(ledA,4);   // turn the LED on (HIGH is the voltage level)
+
+  analogWrite(ledA,4);   
   delay(1000);
-  analogWrite(ledA,12);   // turn the LED on (HIGH is the voltage level)
+  analogWrite(ledA,12);   
   delay(1000);
-  analogWrite(ledA,33);   // turn the LED on (HIGH is the voltage level)
-  analogWrite(ledB,4);   // turn the LED on (HIGH is the voltage level)
+  analogWrite(ledA,33);  
+  analogWrite(ledB,4);  
   delay(1000);
   analogWrite(ledA,64);
-  analogWrite(ledB,12);  // turn the LED on (HIGH is the voltage level)
+  analogWrite(ledB,12);  
   delay(1000);
   analogWrite(ledA,127);
   analogWrite(ledB,33);
-  analogWrite(ledC,4);  // turn the LED on (HIGH is the voltage level)
+  analogWrite(ledC,4);  
   delay(1000);
   analogWrite(ledA,191);
   analogWrite(ledB,64);
-  analogWrite(ledC,12);  // turn the LED on (HIGH is the voltage level)
+  analogWrite(ledC,12);  
   delay(1000);
   analogWrite(ledA,255);
   analogWrite(ledB,127);
@@ -66,28 +58,27 @@ void loop() {
   analogWrite(ledD,191);
   delay(1000);
   analogWrite(ledD,255);
-  // turn the LED on (HIGH is the voltage level)
   delay(10000);  
 
 
 
-  analogWrite(ledA,191);   // turn the LED on (HIGH is the voltage level)
+  analogWrite(ledA,191);
   delay(1000);
-  analogWrite(ledA,127);   // turn the LED on (HIGH is the voltage level)
+  analogWrite(ledA,127);   
   delay(1000);
-  analogWrite(ledA,64);   // turn the LED on (HIGH is the voltage level)
-  analogWrite(ledB,191);   // turn the LED on (HIGH is the voltage level)
+  analogWrite(ledA,64);  
+  analogWrite(ledB,191);  
   delay(1000);
   analogWrite(ledA,33);
-  analogWrite(ledB,127);  // turn the LED on (HIGH is the voltage level)
+  analogWrite(ledB,127);  
   delay(1000);
   analogWrite(ledA,12);
   analogWrite(ledB,64);
-  analogWrite(ledC,191);  // turn the LED on (HIGH is the voltage level)
+  analogWrite(ledC,191);  
   delay(1000);
   analogWrite(ledA,4);
   analogWrite(ledB,33);
-  analogWrite(ledC,127);  // turn the LED on (HIGH is the voltage level)
+  analogWrite(ledC,127);  
   delay(1000);
   analogWrite(ledA,0);
   analogWrite(ledB,12);
@@ -111,7 +102,6 @@ void loop() {
   analogWrite(ledD,4);
   delay(1000);
   analogWrite(ledD,0);
-  // turn the LED on (HIGH is the voltage level)
   delay(10000);  
 
 
